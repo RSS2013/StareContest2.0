f6294cfb101ed65fabee19058947b25f803ef445
Chris Torstenson <c.torsday+github@gmail.com>
2015-05-03 13:31:01 -0700

    frontend not bad

--- canvas_frontend/js/main.js
+++ canvas_frontend/js/main.js
@@ -3,6 +3,16 @@ window.onload = function() {
 
     console.log("YO!");
 
+    var timer_done = false;
+
+    var player_2_conc = 0;
+    var player_2_mellow = 0;
+    var player_2_atk = false;
+
+    var player_1_conc = 0;
+    var player_1_mellow = 0;
+    var player_1_atk = false;
+
     function WebSocketTest() {
         if ("WebSocket" in window) {
             let ws;
@@ -14,9 +24,19 @@ window.onload = function() {
                 // Web Socket is connected, send data using send()
             };
             ws.onmessage = function(evt) {
-                let received_msg;
-                received_msg = evt.data;
-                console.log(received_msg);
+                // let received_msg;
+                var received_msg = evt.data;
+                var obj = JSON.parse(received_msg);
+
+                // reset global (scoff) variables w/ each new message
+                player_1_conc   = obj['Player1']['con'];
+                player_1_mellow = obj['Player1']['mel'];
+                player_1_atk    = obj['Player1']['atk'];
+
+                player_2_conc   = obj['Player2']['con'];
+                player_2_mellow = obj['Player2']['mel'];
+                player_2_atk    = obj['Player2']['atk'];
+
             };
         } else {
             // The browser doesn't support WebSocket
@@ -25,6 +45,12 @@ window.onload = function() {
     }
 
     WebSocketTest();
+
+
+
+
+
+
 
     // ---
 
@@ -38,23 +64,74 @@ window.onload = function() {
     function drawRectangle(myRectangle, context) {
         context.beginPath();
         context.rect(myRectangle.x, myRectangle.y, myRectangle.width, myRectangle.height);
-        context.fillStyle = '#8ED6FF';
+        context.fillStyle = '#222222';
         context.fill();
         context.lineWidth = myRectangle.borderWidth;
         context.strokeStyle = 'black';
         context.stroke();
     }
 
+    // {x, y, radius, start ∠, end ∠, anticlockwise_bool}
+    function drawCircle(myCircle, ctxt) {
+        ctxt.beginPath();
+        ctxt.arc(myCircle.x, myCircle.y, myCircle.radius, myCircle.startAngle, myCircle.endAngle, myCircle.antiClockwise);
+        ctxt.fillStyle = myCircle.color;
+        ctxt.closePath();
+        ctxt.fill();
+        ctxt.lineWidth = myCircle.borderWidth;
+        ctxt.strokeStyle = 'black';
+        ctxt.stroke();
+    }
+
+    function currentRadFor (myCirc) {
+        return player_2_conc;
+    };
+
+    function updateCanvas (canvas, context, startTime) {
+        context.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+
+        circPlayer1Conc.radius = player_1_conc;
+        circPlayer2Conc.radius = player_2_conc;
+        circPlayer1Mellow.radius = player_1_mellow + circPlayer1Conc.radius;
+        circPlayer2Mellow.radius = player_2_mellow + circPlayer2Conc.radius;
+
+        drawCircle(circPlayer1Mellow, context)
+        drawCircle(circPlayer2Mellow, context)
+        drawCircle(circPlayer1Conc, context)
+        drawCircle(circPlayer2Conc, context)
+
+        // request new frame
+        requestAnimFrame(function() {
+            if (timer_done == false) {
+                updateCanvas(canvas, context, startTime);
+            }
+        });
+    };
+
+
+    function updateCircle(myCircle, canvas, context, startTime, newRadius) {
+        myCircle.radius = newRadius;
+        drawCircle(myCircle, context);
+
+        // request new frame
+        requestAnimFrame(function() {
+            updateCircle(myCircle, canvas, context, startTime, currentRadFor(myCircle));
+            // updateCircRadi();
+        });
+    }
+
     function animateTimeBar(myRectangle, canvas, context, startTime) {
         // update
         var time = (new Date()).getTime() - startTime;
 
-        var linearSpeed = 100;
+        var linearSpeed = 20;
         // pixels / second
         var newX = linearSpeed * time / 1000;
 
         if (newX < canvas.width - myRectangle.width - myRectangle.borderWidth / 2) {
             myRectangle.x = newX;
+        } else {
+            timer_done = true;
         }
 
         // clear
@@ -65,14 +142,19 @@ window.onload = function() {
         // request new frame
         requestAnimFrame(function() {
             animateTimeBar(myRectangle, canvas, context, startTime);
+            // updateCircRadi();
         });
     }
 
-    // find the canvas
-    var canvas = document.getElementById('myCanvas');
+    // find the canvas(s)
+    var canvas         = document.getElementById('myCanvas');
+    var timelineCanvas = document.getElementById('timelineCanvas');
+    console.log(canvas);
+    console.log(timelineCanvas);
 
     // access the 2d context (think of it as the brush)
     var context = canvas.getContext('2d');
+    var timelineContext = timelineCanvas.getContext('2d');
 
     // render graphics (x, y, width, height)
     context.fillStyle = 'Green';
@@ -82,25 +164,80 @@ window.onload = function() {
     context.fillStyle = 'Navy';
 
     // (x, y, radius, start ∠, end ∠, antiClockwise)
-    context.arc(200, 200, 50, 0, 2 * Math.PI, false);
+
+    // left, big
+    context.beginPath();
+    context.arc(200, 200, 100, 0, 2 * Math.PI, false);
+    context.closePath();
     context.fill();
 
-    context.arc(600, 200, 50, 0, 2 * Math.PI, false);
+    var circPlayer1Mellow = {
+        x: 200,
+        y: 200,
+        radius: 100,
+        startAngle: 0,
+        endAngle: 2 * Math.PI,
+        antiClockwise: false,
+        color: "Navy"
+    };
+
+    // right, big
+    context.beginPath();
+    context.arc(600, 200, 150, 0, 2 * Math.PI, false);
+    context.closePath();
     context.fill();
+
+    var circPlayer2Mellow = {
+        x: 600,
+        y: 200,
+        radius: 150,
+        startAngle: 0,
+        endAngle: 2 * Math.PI,
+        antiClockwise: false,
+        color: "Navy"
+    };
+
+    // changing brush color
+    context.fillStyle = 'Red';
+
+    var circPlayer1Conc = {
+        x: 200,
+        y: 200,
+        radius: 50,
+        startAngle: 0,
+        endAngle: 2 * Math.PI,
+        antiClockwise: false,
+        color: "Red"
+    };
+
+    var circPlayer2Conc = {
+        x: 600,
+        y: 200,
+        radius: 50,
+        startAngle: 0,
+        endAngle: 2 * Math.PI,
+        antiClockwise: false,
+        color: "Red"
+    };
+
+    drawCircle(circPlayer1Conc, context)
+    drawCircle(circPlayer2Conc, context)
 
     var myRectangle = {
         x: 0,
-        y: 425,
-        width: 100,
+        y: 0,
+        width: 33,
         height: 75,
         borderWidth: 5
     };
 
-    drawRectangle(myRectangle, context);
+    drawRectangle(myRectangle, timelineContext);
 
     // wait one second before starting animation
     setTimeout(function() {
         var startTime = (new Date()).getTime();
-        animateTimeBar(myRectangle, canvas, context, startTime);
-    }, 500);
+        animateTimeBar(myRectangle, timelineCanvas, timelineContext, startTime);
+        // updateCircle(circPlayer2Conc, canvas, context, startTime, circPlayer2Conc.radius);
+        updateCanvas(canvas, context, startTime);
+    }, 500); // 2DO: change to 30000?
 };
