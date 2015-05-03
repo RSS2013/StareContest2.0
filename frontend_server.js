9a03b564e35faadd8ad0931fc9f289bc7b37e4e0
Chris Torstenson <c.torsday+github@gmail.com>
2015-05-03 13:30:42 -0700

    better mock server data

--- frontend_server.js
+++ frontend_server.js
@@ -6,13 +6,26 @@
     wss             = new WebSocketServer({ port: 8080 });
 
     wss.on('connection', function connection(ws) {
-        
+
         ws.on('message', function incoming(message) {
             console.log('received: %s', { "test_key": "test_val"});
         });
 
+
         setInterval(function() {
-            ws.send("Message to send");
+            j_blob = JSON.stringify({
+                "Player1" : {
+                    "con": Math.floor(Math.random() * 100),
+                    "mel": Math.floor(Math.random() * 100),
+                    "atk": (Math.floor(Math.random() * 6) == 1),
+                },
+                "Player2" : {
+                    "con": Math.floor(Math.random() * 100),
+                    "mel": Math.floor(Math.random() * 100),
+                    "atk": (Math.floor(Math.random() * 6) == 1),
+                }
+            });
+            ws.send(j_blob);
         }, 500);
     });
 
